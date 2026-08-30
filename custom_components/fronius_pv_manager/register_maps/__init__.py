f69553ce2d7b53946ce1fac1c0849a21276f3cb4
Ekkehard Lutz <ekkehard.lutz@a1.net>
2026-08-30 22:07:13 +0200

    Add SunSpec model 121 definition

--- custom_components/fronius_pv_manager/register_maps/__init__.py
+++ custom_components/fronius_pv_manager/register_maps/__init__.py
@@ -3,6 +3,7 @@
 from .model_1 import MODEL_1
 from .model_103 import MODEL_103
 from .model_120 import MODEL_120
+from .model_121 import MODEL_121
 from .model_160 import MODEL_160
 
-__all__ = ["MODEL_1", "MODEL_103", "MODEL_120", "MODEL_160"]
+__all__ = ["MODEL_1", "MODEL_103", "MODEL_120", "MODEL_121", "MODEL_160"]

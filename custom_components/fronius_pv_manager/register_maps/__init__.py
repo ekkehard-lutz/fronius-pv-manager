2cc80c7d1eea12116e1975e08742e389b60c9685
Ekkehard Lutz <ekkehard.lutz@a1.net>
2026-08-30 21:53:52 +0200

    Add SunSpec common model 1 definition

--- custom_components/fronius_pv_manager/register_maps/__init__.py
+++ custom_components/fronius_pv_manager/register_maps/__init__.py
@@ -1,6 +1,7 @@
 """Reviewed SunSpec register-map definitions shipped by the integration."""
 
+from .model_1 import MODEL_1
 from .model_103 import MODEL_103
 from .model_160 import MODEL_160
 
-__all__ = ["MODEL_103", "MODEL_160"]
+__all__ = ["MODEL_1", "MODEL_103", "MODEL_160"]
